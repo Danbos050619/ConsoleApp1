63c5c2d89fb024e89c335ca49e00ac8010c99ec2
DanielBostouh <danbos050619@student.jenseneducation.se>
2022-10-18 13:33:08 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+
+{
+public class Bil
+        private int hjul = 4;
+    private string färg = "vit";
+    private string Bränsle = "Diesel";
+
+    public int
+}
